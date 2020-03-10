76400cb58e6644d3b06a3890a4b42ef5b68e7a84
Mauricio Cruz Portilla <mauricioportilla189@hotmail.com>
2020-03-09 19:06:27 -0600

    Shell script fixed

--- provision/box_setup.sh
+++ provision/box_setup.sh
@@ -8,19 +8,24 @@ update_packages() {
 
 install_php() {
     echo "Installing PHP"
+    sudo apt-get install software-properties-common
     sudo add-apt-repository ppa:ondrej/php
+    sudo add-apt-repository ppa:ondrej/apache2
     sudo apt-get update
-    sudo apt install php7.3
+    sudo apt-get upgrade
+    sudo apt-get install -y php7.3
     sudo systemctl restart apache2
 }
 
 install_python() {
+    echo "Installing Python"
     sudo add-apt-repository ppa:deadsnakes/ppa
     sudo apt-get update
     sudo apt install python3.7
 }
 
 clean_up() {
+    echo "Cleaning"
     sudo apt -y autoremove && sudo apt autoclean > /dev/null 2>&1
 }
 
