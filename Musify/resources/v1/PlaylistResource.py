a427f71c970d85c705f1595a8d4f6c3593c2da8c
Mauricio Cruz Portilla <mauricioportilla189@hotmail.com>
2020-06-27 14:37:16 -0500

    Playlist resource PUT added. gRPC server files removed

--- Musify/resources/v1/PlaylistResource.py
+++ Musify/resources/v1/PlaylistResource.py
@@ -25,6 +25,22 @@ class PlaylistResource(Resource):
         return { "status": "success", "data": result }, 201
 
     @auth_token
+    def put(self, account, playlist_id):
+        json_data = request.get_json()
+        if not json_data:
+            return { "status": "failed", "message": "No input data provided." }, 400
+        if len(json_data["name"]) == 0 or str.isspace(json_data["name"]):
+            return { "status": "failed", "message": "No valid input data provided." }, 400
+        playlist = Playlist.query.filter_by(playlist_id=playlist_id).first()
+        if not playlist:
+            return { "status": "failed", "message": "This playlist does not exist." }, 422
+        if account.account_id != playlist.account_id:
+            return { "status": "failed", "message": "Unauthorized." }, 401
+        playlist.name = json_data["name"]
+        database.session.commit()
+        return { "status": "success", "data": playlist_schema.dump(playlist).data }, 201
+
+    @auth_token
     def delete(self, account, playlist_id):
         playlist = Playlist.query.filter_by(playlist_id=playlist_id).first()
         if not playlist:
